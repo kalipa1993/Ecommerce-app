de4efccce3c3834f598f9b7c6eceb596ee4401ac
Esteban Retamales <fire211993@gmail.com>
2022-07-18 19:22:38 -0400

    contador

---
+++ src/componentes/ItemCount/index.jsx
@@ -0,0 +1,31 @@
+import "./ItemCount.css";
+
+import React,{useState} from "react";
+
+export const ItemCount = ({initial,stock,onAdd}) => {
+    const [count,setCount] = useState(initial);
+    const decrease =() => {
+        setCount(count - 1);
+    }
+    
+        const increase =() => {
+            setCount(count + 1);
+        }
+    
+
+    return (
+        <div className="contador">
+            <button disabled={count <= 1} onClick={decrease}>-</button>
+            <span>{count}</span>
+            <button disabled ={count => stock} onClick={increase}>+</button>
+        <div>
+        
+          <button disabled={stock <= 0} onClick>Agregar Al Carrito</button>
+         </div>
+         </div>
+
+         
+    );
+} 
+
+export default ItemCount;
--- src/componentes/Navbar/index.jsx
+++ src/componentes/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+
 import Cartwidget from "../Cartwidget";
 
 
