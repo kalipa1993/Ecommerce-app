8080a977e9bf1e9b3f101f98bbb09f078edd79a4
Esteban Retamales <fire211993@gmail.com>
2022-07-20 22:26:40 -0400

    promise

--- src/componentes/ItemListContainer/index.jsx
+++ src/componentes/ItemListContainer/index.jsx
@@ -1,15 +1,86 @@
-import React from "react";
+import React,{useEffect,useState}from "react";
 import Title from "../Title";
 import ItemCount from "../ItemCount";
-const ItemListContainer = () => {
+import ItemList from "../ItemList";
+
+const productos =[
+    {
+        "idLoja":001,
+        "departamento": "PERECIVEIS", 
+        "categoria": "FRIOS",
+        "subCategoria": "APRESUNTADO",
+        "marca": "SADIA",
+        "unidade": "KG",
+        "volume": "1KG",
+        "codigoBarra": "7891231231231",
+        "nome": "APRESENTURADO SADIA 1KG",
+        "valor": 19.99,
+        "valorPromocao": 14.99,
+        "valorAtacado": 0,
+        "valorCompra": 0,
+        "quantidadeEstoqueAtual": 5.492, 
+        "quantidadeEstoqueMinimo": 0,
+        "quantidadeAtacado": 0,
+        "descricao": "APRESENTURADO SADIA 1KG",
+        "ativo": true,
+        "plu": "101010",
+        "validadeProxima": false,
+        "imageURL": "https://site.com/imagem.jpg"
+      
+    },
+
+    {
+        "idLoja":002,
+        "departamento": "PERECIVEIS", 
+        "categoria": "FRIOS",
+        "subCategoria": "APRESUNTADO",
+        "marca": "SADIA",
+        "unidade": "KG",
+        "volume": "1KG",
+        "codigoBarra": "7891231231231",
+        "nome": "APRESENTURADO SADIA 1KG",
+        "valor": 19.99,
+        "valorPromocao": 14.99,
+        "valorAtacado": 0,
+        "valorCompra": 0,
+        "quantidadeEstoqueAtual": 5.492, 
+        "quantidadeEstoqueMinimo": 0,
+        "quantidadeAtacado": 0,
+        "descricao": "APRESENTURADO SADIA 1KG",
+        "ativo": true,
+        "plu": "101010",
+        "validadeProxima": false,
+        "imageURL": "https://site.com/imagem.jpg"
+      }
+    
+];
+   
+
+   export const ItemListContainer = ({ texto}) => {
+    
+     const [data,setData] = useState ([]);
+
+     useEffect =(() => {
+      const getData = new Promise (resolve => {
+        setTimeout(() => {
+          resolve (productos);
+        },3000);
+
+       getData.then(res => setData(res));
+
+      });
+     }, [])
+
     const onAdd =(quantity) => {
         console.log(`compraste $ {qauntity} unidades`);
     }
+   }
     return(
         <>
        <Title/>
        <ItemCount initial ={1} stock = {5} onAdd={onAdd}/>
+       <ItemList data ={data}/>
        </>
     );
-}
+
  export default ItemListContainer;
